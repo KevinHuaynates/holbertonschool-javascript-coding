7a48d86ba292c6cbe0a7fa5b16919432dd6b6e34
KevinHuaynates <6992@holbertonstudents.com>
2024-02-15 21:54:30 -0800

    2

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Importa el módulo fs
+const fs = require('fs');
+
+// Obtiene la ruta del archivo y el contenido a escribir de los argumentos de la línea de comandos
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Escribe el contenido en el archivo especificado
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  // Maneja los errores, si los hay
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(`Content has been written to ${filePath}`);
+});
