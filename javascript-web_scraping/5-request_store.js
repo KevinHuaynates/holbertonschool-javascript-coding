22a9971a6fe1ba54dfd21695b0e2df0d61fa0460
KevinHuaynates <6992@holbertonstudents.com>
2024-02-15 22:19:27 -0800

    6

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+// Obtén la URL de la solicitud y la ruta del archivo de los argumentos de la línea de comandos
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Realiza una solicitud GET a la URL proporcionada
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  // Escribe el contenido de la respuesta en el archivo especificado
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`File ${filePath} saved!`);
+  });
+});
