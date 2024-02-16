991057ff3e51b5c676764c3dd9a779f355f02c6e
KevinHuaynates <6992@holbertonstudents.com>
2024-02-15 22:07:02 -0800

    1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,25 +1,14 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-// Verifica si se proporcionan los argumentos necesarios
-if (process.argv.length !== 4) {
-  console.error('Usage: ./1-writeme.js <file-path> "<string-to-write>"');
-  process.exit(1);
-}
-
-// Obtiene la ruta del archivo y el contenido a escribir de los argumentos de la línea de comandos
+// Get the file path and string from the command line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-// Escribe el contenido en el archivo especificado
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  // Maneja los errores, si los hay
+// Write the string to the file
+fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-
-  // Imprime el contenido del archivo escrito
-  console.log(content);
 });
