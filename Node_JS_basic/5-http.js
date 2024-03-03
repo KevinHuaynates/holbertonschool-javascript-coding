d624ec3ade0a4a199efdeb15f10165a20e04b340
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 15:50:08 -0800

    5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,14 +1,12 @@
-#!/usr/bin/node
 const http = require('http');
 const fs = require('fs');
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
-    res.statusCode = 200;
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.statusCode = 200;
     fs.readFile(process.argv[2], 'utf8', (err, data) => {
       if (err) {
         res.statusCode = 500;
@@ -26,15 +24,19 @@ const app = http.createServer((req, res) => {
             fields[field].push(student[0]);
           }
         });
+
         const totalStudents = students.length - 1;
-        res.write(`This is the list of our students\n`);
-        res.write(`Number of students: ${totalStudents}\n`);
+
+        // Combine content into a single string
+        const response = `This is the list of our students\n` +
+                         `Number of students: ${totalStudents}\n`;
         for (const field in fields) {
           if (Object.prototype.hasOwnProperty.call(fields, field)) {
-            res.write(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`);
+            response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
           }
         }
-        res.end();
+
+        res.end(response);
       }
     });
   } else {
