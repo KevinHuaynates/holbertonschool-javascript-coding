0348c57744194237b22f22dc5e201b2f6b516a58
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 15:47:26 -0800

    5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,48 +1,52 @@
-#!/usr/bin/node
 const http = require('http');
 const fs = require('fs');
 
 const app = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.statusCode = 200;
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.statusCode = 200;
-    fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    countStudents(process.argv[2], (err, data) => {
       if (err) {
-        res.statusCode = 500;
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Cannot load the database');
       } else {
-        const lines = data.trim().split('\n');
-        const students = lines.map((line) => line.split(','));
-        const fields = {};
-        students.forEach((student) => {
-          if (student.length === 4 && student[0] !== 'firstname') {
-            const field = student[3];
-            if (!fields[field]) {
-              fields[field] = [];
-            }
-            fields[field].push(student[0]);
-          }
-        });
-        const totalStudents = students.length - 1;
-        res.write(`This is the list of our students\n`);
-        res.write(`Number of students: ${totalStudents}\n`);
-        for (const field in fields) {
-          if (Object.prototype.hasOwnProperty.call(fields, field)) {
-            res.write(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`);
-          }
-        }
-        res.end();
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(data);
       }
     });
   } else {
-    res.statusCode = 404;
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not found');
   }
 });
 
 app.listen(1245);
 
-module.exports = app;
+function countStudents(path, callback) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      callback(new Error('Cannot load the database'));
+    } else {
+      const lines = data.trim().split('\n');
+      const students = lines.map((line) => line.split(','));
+      const fields = {};
+      students.forEach((student) => {
+        if (student.length === 4 && student[0] !== 'firstname') {
+          const field = student[3];
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(student[0]);
+        }
+      });
+      const totalStudents = students.length - 1;
+      callback(null, `Number of students: ${totalStudents}\n`);
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          callback(null, `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`);
+        }
+      }
+    }
+  });
+}
