2c2c533eda1f6cb858cc329cefd7375db63cee92
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 14:08:28 -0800

    2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 // 1-stdin.js
 
 // Mostrar el mensaje de bienvenida
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 // Configurar la entrada estándar (stdin) para leer desde la consola
 process.stdin.setEncoding('utf8');
@@ -11,12 +11,12 @@ process.stdin.on('data', function(data) {
     // Eliminar el salto de línea del final del input
     const name = data.trim();
 
-    // Mostrar el nombre ingresado por el usuario
-    console.log("Your name is:", name);
+    // Mostrar el nombre ingresado por el usuario con un salto de línea
+    process.stdout.write(`Your name is: ${name}\n`);
 });
 
 // Manejar el evento 'end' cuando el usuario termina el programa
 process.stdin.on('end', function() {
-    // Mostrar el mensaje de cierre
-    console.log("This important software is now closing");
+    // Mostrar el mensaje de cierre con un salto de línea
+    process.stdout.write("This important software is now closing\n");
 });
