f54b817be5142f4369976e73ef69019ef0dd33fe
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 15:15:28 -0800

    4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,17 +1,12 @@
-// Importing the required module
+#!/usr/bin/node
 const http = require('http');
 
-// Creating a HTTP server
 const app = http.createServer((req, res) => {
-    // Setting the response headers
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // Sending the response
-    res.end('Hello Holberton School!\n');
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
 });
 
-// Listening on port 1245
 app.listen(1245);
 
-// Exporting the app
 module.exports = app;
