6e5303481ea86e0f421d1c0d239a8f5e8e688069
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 13:59:10 -0800

    2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const name = data.trim(); // Elimina espacios en blanco alrededor del nombre
-  console.log(`Your name is: ${name}\r`);
+  console.log(`Your name is: ${name}\r\n`); // Añade \r\n al final del mensaje
   console.log('This important software is now closing');
   process.exit(); // Termina el programa
 });
