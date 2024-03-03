d2ea146c3df1bb2ef831b5d391fd971ac57293aa
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 14:44:13 -0800

    5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -8,16 +8,21 @@ const app = http.createServer((req, res) => {
     res.end('Hello Holberton School!\n');
   } else if (req.url === '/students') {
     const database = process.argv[2];
-    countStudents(database)
-      .then(() => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        const data = fs.readFileSync(database, 'utf8');
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((err) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(err.message);
-      });
+    if (!database) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Database file not provided\n');
+    } else {
+      countStudents(database)
+        .then(() => {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          const data = fs.readFileSync(database, 'utf8');
+          res.end(`This is the list of our students\n${data}`);
+        })
+        .catch((err) => {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(err.message);
+        });
+    }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found\n');
