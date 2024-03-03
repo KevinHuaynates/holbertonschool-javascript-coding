29067726e073c2b6821228dbd5228361513e74f4
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 14:19:54 -0800

    2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,39 +4,32 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    // Lee el archivo de manera síncrona
     const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
 
-    // Divide el archivo en líneas
-    const lines = data.split('\n').filter(line => line.trim() !== ''); // Ignora líneas vacías
+    const students = lines.map(line => line.split(',')).filter(student => student.length === 4);
 
-    // Inicializa contadores para cada campo
-    const counters = {};
-
-    // Itera sobre cada línea del archivo
-    lines.forEach(line => {
-      const [, , , field] = line.split(','); // Ignora el primer campo (nombre) y el segundo campo (apellido)
-      if (counters[field]) {
-        counters[field]++;
+    const fields = {};
+    students.forEach(student => {
+      const field = student[3];
+      const name = student[0];
+      if (fields[field]) {
+        fields[field].count++;
+        fields[field].students.push(name);
       } else {
-        counters[field] = 1;
+        fields[field] = {
+          count: 1,
+          students: [name]
+        };
       }
     });
 
-    // Imprime el número total de estudiantes
-    const totalStudents = lines.length;
-    console.log(`Number of students: ${totalStudents}`);
-
-    // Imprime el número de estudiantes en cada campo y su lista de nombres
-    Object.entries(counters).forEach(([field, count]) => {
-      const studentsList = lines
-        .filter(line => line.split(',')[3] === field) // Filtra las líneas por campo
-        .map(line => line.split(',')[0]); // Extrae los nombres de los estudiantes
-      console.log(`Number of students in ${field}: ${count}. List: ${studentsList.join(', ')}`);
-    });
+    console.log(`Number of students: ${students.length}`);
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].students.join(', ')}`);
+    }
   } catch (error) {
-    // Captura y maneja cualquier error al leer el archivo
-    console.error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 
