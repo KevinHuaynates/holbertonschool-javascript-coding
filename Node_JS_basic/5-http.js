39e5119aa55fb1173ebd322f050d97ffabc41707
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 14:42:08 -0800

    5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const { countStudents } = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    const database = process.argv[2];
+    countStudents(database)
+      .then(() => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        const data = fs.readFileSync(database, 'utf8');
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((err) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(err.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
