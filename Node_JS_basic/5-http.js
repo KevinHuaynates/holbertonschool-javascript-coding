cb03d23e7c653d45710355cca9c57b1dc2f6c7e9
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 15:35:51 -0800

    5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,46 +1,48 @@
+#!/usr/bin/node
 const http = require('http');
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-function handleRequest(req, res) {
-  switch (req.url) {
-    case '/':
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello Holberton School!');
-      break;
-    case '/students':
-      readFileAsync(filePath)
-        .then(() => {
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          const stream = fs.createReadStream(filePath);
-          stream.pipe(res);
-        })
-        .catch((error) => {
-          console.error(error);
-          res.writeHead(500);
-          res.end('Error reading file');
-        });
-      break;
-    default:
-      res.writeHead(404);
-      res.end('Not found');
-  }
-}
-
-function readFileAsync(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    fs.readFile(process.argv[2], 'utf8', (err, data) => {
       if (err) {
-        reject(err);
+        res.statusCode = 500;
+        res.end('Cannot load the database');
       } else {
-        resolve(data);
+        const lines = data.trim().split('\n');
+        const students = lines.map((line) => line.split(','));
+        const fields = {};
+        students.forEach((student) => {
+          if (student.length === 4 && student[0] !== 'firstname') {
+            const field = student[3];
+            if (!fields[field]) {
+              fields[field] = [];
+            }
+            fields[field].push(student[0]);
+          }
+        });
+        const totalStudents = students.length - 1;
+        res.write(`This is the list of our students\n`);
+        res.write(`Number of students: ${totalStudents}\n`);
+        for (const field in fields) {
+          if (Object.prototype.hasOwnProperty.call(fields, field)) {
+            res.write(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`);
+          }
+        }
+        res.end();
       }
     });
-  });
-}
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
 
-const app = http.createServer(handleRequest);
 app.listen(1245);
 
 module.exports = app;
