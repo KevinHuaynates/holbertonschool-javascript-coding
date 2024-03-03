617967bf4a35042b8d2c1bc26fca95a22c483151
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 14:47:16 -0800

    7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,15 +1,3 @@
-const express = require('express');
-const { countStudents } = require('./3-read_file_async');
-const fs = require('fs');
-
-const app = express();
-
-const PORT = 1245;
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
 app.get('/students', (req, res) => {
   const database = req.query.database;
   if (!database) {
@@ -19,15 +7,9 @@ app.get('/students', (req, res) => {
   countStudents(database)
     .then(() => {
       const data = fs.readFileSync(database, 'utf8');
-      res.send(`This is the list of our students\n${data}`);
+      res.status(200).send(`This is the list of our students\n${data}`);
     })
     .catch((err) => {
       res.status(500).send(err.message);
     });
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-module.exports = app;
