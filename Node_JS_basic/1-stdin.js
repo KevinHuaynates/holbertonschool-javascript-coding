90660735162379b70d29952de0c3efcd039fab28
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 15:53:38 -0800

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,4 @@
-#!/usr/bin/env node
-
-process.stdin.resume();
-
-process.stdout.write('Welcome to Holberton School, what is your name?\r');
-
-process.stdin.on('data', (input) => {
-  process.stdout.write(`Your name is: ${input.toString().trim()}\r`);
-  process.exit(0);
-});
-
-process.on('close', () => {
-  console.log('This important software is now closing');
-});
+const { stdin, stdout } = require('process');
+stdout.write('Welcome to Holberton School, what is your name?\n');
+stdin.on('data', data => stdout.write(`Your name is: ${data}`));
+stdin.on('end', () => stdout.write('This important software is now closing\n'));
