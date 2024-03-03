eec6a3ed1cf38281ac76a476572dc3d2059ceb92
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 14:40:48 -0800

    4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,6 +4,9 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!\n');
+  } else if (req.url === '/test') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Test\n');
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found\n');
